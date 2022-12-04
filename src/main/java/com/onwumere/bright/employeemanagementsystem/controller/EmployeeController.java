4ee449db88f6853284a81771524783ece86488f5
Okechukwu Bright Onwumere <brighthamza9@yahoo.com>
2022-12-04 17:25:36 +0100

    Initial Commit for the Employee Management System Web Application

---
+++ src/main/java/com/onwumere/bright/employeemanagementsystem/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.onwumere.bright.employeemanagementsystem.controller;
+/*
+ * @Project Name: employee-management-system
+ * @Author: Okechukwu Bright Onwumere
+ * @Created: 04/12/2022
+ */
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping
+    public String getEmployee(){
+        return "employee";
+    }
+
+}
